c841a604fed07be98cfe2ce2e6ee9c3ef18c309f
Petar Petrov <petar.petrov.georgiev@gmail.com>
2013-06-26 20:58:21 +0200

    Edited license message in src files.

--- src/utils.h
+++ src/utils.h
@@ -1,26 +1,20 @@
 /*
-  utils.h
-  This file is part of e-additives.server
-
-  Copyright (C) 2012 Petar Petrov
-
-  This program is free software; you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation; either version 2 of the License, or
-  (at your option) any later version.
-
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-  GNU General Public License for more details.
-
-  You should have received a copy of the GNU General Public License
-  along with this program; if not, write to the Free Software
-  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
-
-  For more information please contact:
-  petar.petrov.georgiev(at)gmail[dot]com
-
+ * Strelko is a small-footprint HTTP server.
+ * Copyright (C) 2013 Petar Petrov
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
 */
 
 #ifndef UTILS_H_
